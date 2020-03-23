a24024e9b9023ed6e663a1a696361515bba493c8
helgoyat <goyat.helene@gmail.com>
2020-03-23 16:28:29 -0400

    Start of findPath func

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ sample = [
     [0, 0, 1, 1]
 ]
 
-# start = {x: 0, y: 0}
-# end = {x: 3, y: 0}
+start = {'x': 0, 'y': 0}
+end = {'x': 3, 'y': 0}
 
-myMaze = maze.Maze(sample, 0, 0)
+myMaze = maze.Maze(sample, start, end)
 myMaze.solve()
--- maze.py
+++ maze.py
@@ -2,9 +2,26 @@ class Maze():
     def __init__(self, maze, start, end):
         self.size = 4
         self.maze = maze
-        self.start = start
-        self.end = end
+        self.start = {'x': start['x'], 'y': start['y']}
+        self.end = {'x': end['x'], 'y': end['y']}
         self.solutions = []
 
     def solve(self):
-        print('I am a maze')
+
+        trace = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]
+        ]
+
+        self.findPath(self.start, 0, trace)
+
+    def findPath(self, cursor, count, trace):
+
+        value = self.maze[cursor['x']][cursor['y']]
+        isPath = (value == 0)
+        isVisited = (trace[cursor['x']][cursor['y']] > 0)
+
+        if isPath and not(isVisited):
+            print(value)
